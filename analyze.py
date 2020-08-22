19c668487386ac446d6990044500d0d48060b6c2
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2020-08-22 17:28:00 -0600

    extract time boxes

--- analyze.py
+++ analyze.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 with open('data/database.json') as f:
   allPages = json.load(f)
@@ -8,12 +9,48 @@ summer_term = ("May", "June", "July", "August")
 time_range = summer_term
 year = "2020"
     
+hhmmStartsWithCheck = re.compile("^([0-1]?[0-9]|2[0-3]):[0-5][0-9]")
+hhmmStrictCheck = re.compile("^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$")
+
 page_count = 0
 for page in allPages:    
-    pageTitle = page.get("title")    
+    pageTitle = page.get("title")
+
+    # Filter out daily pages
     if pageTitle.startswith(time_range) and pageTitle.endswith(year):
-        print(page.get("title"))      
-        page_count += 1      
+        # print(page.get("title"))   
+        notes = page.get("children")
+
+        # If page isn't empty
+        if notes is not None:
+            for note in notes:
+                # Get time boxes
+                parentTitle = note.get("string")
+                
+                # If time tracking parent note
+                if hhmmStartsWithCheck.match(parentTitle):
+                    # If end time exists                    
+                    endTimeStartIndex = re.search(r"\d", parentTitle[5:])
+                    if endTimeStartIndex is not None:                        
+                        print(parentTitle)
+                        
+                        startTime = parentTitle[0:5]
+                        assert(hhmmStrictCheck.match(startTime))
+
+                        # print(startTime)           
+                        
+                        endTimeStartIndex = endTimeStartIndex.start() + 5
+                        endTime = parentTitle[endTimeStartIndex:endTimeStartIndex+5]
+                        assert(hhmmStrictCheck.match(endTime))
+
+                        # print(endTime)           
+                        
+                        
+                        # break            
+    
+            page_count += 1
+        
+        # break
 
 print("-----------")
 print("Analyzed: " + str(page_count) + " pages")
