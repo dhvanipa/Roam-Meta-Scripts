352b095be47781627ca37630e6bcf3540ec2bd21
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2020-09-07 15:40:28 -0600

    add linter

--- analyze.py
+++ analyze.py
@@ -4,15 +4,14 @@ from datetime import datetime
 from datetime import timedelta
 import matplotlib.pyplot as plt
 
-
 with open('data/database.json') as f:
-  allPages = json.load(f)
+    allPages = json.load(f)
 
 summerTerm = ("May", "June", "July", "August")
 
 timeRange = summerTerm
 year = "2020"
-    
+
 hhmmStartsWithCheck = re.compile("^([0-1]?[0-9]|2[0-3]):[0-5][0-9]")
 hhmmStrictCheck = re.compile("^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$")
 
@@ -20,37 +19,37 @@ dates = ["Jan 1", "Jan 2"]
 timeSpent = [3, 4]
 
 categories = [
-    "thinking",    
-    "transition",    
+    "thinking",
+    "transition",
     "wandering",
-    "procrastination",    
+    "procrastination",
     "workout",
     "family",
     "friends",
     "lunch",
     "dinner",
-    "morning routine",                               
+    "morning routine",
     "info consumption",
     "research",
     "errands",
     "planning",
-    "reflecting",   
+    "reflecting",
     "break",
-    "school",    
+    "school",
     "coding",
     "walk",
     "reading",
     "yc",
-    "co-op",    
+    "co-op",
     "bookmark",
-    "wandcrafting", 
+    "wandcrafting",
     "reflection",
 ]
 
 categoriesRaw = [
     "thinking",
     "think",
-    "transition",    
+    "transition",
     "wandering",
     "procrastination",
     "work out",
@@ -63,9 +62,9 @@ categoriesRaw = [
     "morning exercise. shower. get ready",
     "shower. get ready",
     "shower. morning exercise.",
-    "shower and get ready.",    
+    "shower and get ready.",
     "get ready",
-    "morning exercise. shower.",    
+    "morning exercise. shower.",
     "setup",
     "info consumption",
     "research",
@@ -85,7 +84,7 @@ categoriesRaw = [
     "ece192",
     "ece 192",
     "break",
-    "school",    
+    "school",
     "coding",
     "walk",
     "reading",
@@ -94,18 +93,18 @@ categoriesRaw = [
     "coop",
     "bookmark",
     "wandcrafting",
-    "wandservers",    
+    "wandservers",
     "reflection",
 ]
 
 pageCount = 0
 
-for page in allPages:    
+for page in allPages:
     pageTitle = page.get("title")
 
     # Filter out daily pages
     if pageTitle.startswith(timeRange) and pageTitle.endswith(year):
-        # print(page.get("title"))   
+        # print(page.get("title"))
         notes = page.get("children")
 
         # If page isn't empty
@@ -113,50 +112,52 @@ for page in allPages:
             for note in notes:
                 # Get time boxes
                 parentTitle = note.get("string")
-                
+
                 # If time tracking parent note
                 if hhmmStartsWithCheck.match(parentTitle):
-                    # If end time exists                    
+                    # If end time exists
                     endTimeStartIndex = re.search(r"\d", parentTitle[5:])
                     if endTimeStartIndex is not None:
                         # print(parentTitle)
-                        
+
                         startTime = parentTitle[0:5]
                         assert(hhmmStrictCheck.match(startTime))
 
-                        # print(startTime)           
-                        
+                        # print(startTime)
+
                         endTimeStartIndex = endTimeStartIndex.start() + 5
                         endTime = parentTitle[endTimeStartIndex:endTimeStartIndex+5]
                         assert(hhmmStrictCheck.match(endTime))
 
-                        # print(endTime)           
+                        # print(endTime)
 
                         FMT = '%H:%M'
-                        tdelta = datetime.strptime(endTime, FMT) - datetime.strptime(startTime, FMT)
+                        tdelta = datetime.strptime(
+                            endTime, FMT) - datetime.strptime(startTime, FMT)
                         if tdelta.days < 0:
-                            tdelta = timedelta(days=0, seconds=tdelta.seconds, microseconds=tdelta.microseconds)
+                            tdelta = timedelta(
+                                days=0, seconds=tdelta.seconds, microseconds=tdelta.microseconds)
 
-                        task = parentTitle[endTimeStartIndex+5:]                        
-                        task = task.lower()                        
+                        task = parentTitle[endTimeStartIndex+5:]
+                        task = task.lower()
 
                         if not any(map(task.__contains__, categoriesRaw)):
                             print("FIX TAG: ")
-                            print(page.get("title"))   
+                            print(page.get("title"))
                             print(task)
                         else:
                             for rawCat in categoriesRaw:
                                 if rawCat.find(task):
-                                    print(rawCat)                        
+                                    print(rawCat)
 
                             # print(tdelta)
-                            # hours = tdelta.seconds//3600 
-                            # minutes = (td.seconds//60)%60                       
-                      
-                        # break            
-    
+                            # hours = tdelta.seconds//3600
+                            # minutes = (td.seconds//60)%60
+
+                        # break
+
             pageCount += 1
-        
+
         # break
 
 print("-----------")
